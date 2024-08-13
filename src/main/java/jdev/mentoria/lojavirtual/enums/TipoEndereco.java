064578743198345d0ddab9a725fa175d05b2aa25
marco <assuncaofilho@gmail.com>
2024-08-12 23:18:23 -0300

    Impl de TipoEndereco ENUM - alter table Endereco add column tipoEndereco

---
+++ src/main/java/jdev/mentoria/lojavirtual/enums/TipoEndereco.java
@@ -0,0 +1,23 @@
+package jdev.mentoria.lojavirtual.enums;
+
+public enum TipoEndereco {
+	
+	COBRANCA("Cobranca"),
+	ENTREGA("Entrega");
+	
+	String descricao;
+
+	private TipoEndereco(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	@Override
+	public String toString() {
+		return descricao;
+	}
+
+}
